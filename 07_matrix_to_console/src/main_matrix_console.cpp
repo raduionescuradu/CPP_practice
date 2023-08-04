7d2641b554dd5660a40483b1f3ac87b46455a0f5
Radu Ionescu <radu.ionescuradu@gmail.com>
2023-08-04 17:13:01 +0300

    solved 07_matrix_to_console

--- 07_matrix_to_console/src/main_matrix_console.cpp
+++ 07_matrix_to_console/src/main_matrix_console.cpp
@@ -11,18 +11,22 @@ private:
 	size_t column_count;
 	size_t line_count;
 
+	std::vector<std::string> matrice;
 	// TODO: store the data
 	// hints: you can use std::string, std::vectors + string, char**, vector<vector<char>>, etc
 public:
 	Matrix(size_t numColumnsX, size_t numLinesY)
 		// TODO: add functionality
 	{
+		this->column_count = numColumnsX;
+		this->line_count = numLinesY;
 		// TODO: add functionality
 	}
 
 	// Set an entire line
 	void setLine(size_t line_number, const std::string& data)
 	{
+		auto it = matrice.emplace(matrice.begin() + line_number, data);
 	}
 
 	//OPTIONAL
@@ -55,12 +59,16 @@ public:
 	void setCellXY(size_t x, size_t y, char cell_content)
 	{
 		// TODO: add functionality
+		if (x < this->column_count && y < this->line_count)
+			matrice[y][x] = cell_content;
 	}
 
 	void print()
 	{
 		// print all lines and columns
 		// TODO: add functionality
+		for (int i = 0; i < this->line_count; i++)
+			std::cout << this->matrice[i] << std::endl;
 	}
 };
 
